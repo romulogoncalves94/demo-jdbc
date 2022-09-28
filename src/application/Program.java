57a2f8fdfa05ab6fc9c96959e2b1d2a22e357ab8
romulogoncalves94 <romulogoncalves94@hotmail.com>
2022-09-28 12:05:35 -0300

    feat: criado findAll

--- src/application/Program.java
+++ src/application/Program.java
@@ -23,5 +23,11 @@ public class Program {
 		for(Seller obj : list) {
 			System.out.println(obj);
 		}
+		
+		System.out.println("\n=== TESTE 3: seller findByAll ===");
+		list = sellerDao.findAll();
+		for(Seller obj : list) {
+			System.out.println(obj);
+		}
 	}
 }
